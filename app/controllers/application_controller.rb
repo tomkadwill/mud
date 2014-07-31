ed766c339e4c1398116333f1b7bd8f79cf1ce4bb
Tom Kadwill <tomkadwill@gmail.com>
2014-07-31 22:32:29 +0100

    stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :redirect_to_user
+
+  private
+
+  def redirect_to_user
+    redirect_to 'http://www.google.com'
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_filter :authenticate_user!
+  before_action :call_logger2
 
   def index
     @users = User.all
@@ -9,4 +10,9 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  private
+
+  def call_logger2
+    logger.debug 'UsersController log'
+  end
 end
