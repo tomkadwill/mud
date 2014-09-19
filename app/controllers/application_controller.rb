a2f7fac07492c67223373f44f7a78700e90a5eb3
Tom Kadwill <tomkadwill@gmail.com>
2014-09-19 06:33:16 +0100

    various fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :redirect_to_user
-
   private
 
-  def redirect_to_user
-    redirect_to 'http://www.google.com'
-  end
 end
